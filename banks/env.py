e913f6423c1072e06c8828fb17bf9ec95096fe63
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2023-05-13 23:35:34 +0200

    Automatically create .env file if missing (fixes #3)

--- banks/env.py
+++ banks/env.py
@@ -2,4 +2,12 @@
 import dotenv
 
 dotenv_file = dotenv.find_dotenv()
+if not dotenv_file:
+    # create empty .env file
+    with open(".env", "w") as fp:
+        fp.write("")
+    dotenv_file = dotenv.find_dotenv()
+    if not dotenv_file:
+        print("Something is really wrong, .env not found after being created")
+        exit(1)
 dotenv.load_dotenv(dotenv_file)
