6b11d32e0515921133b5d7c7762fa97933f635ab
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2023-05-25 14:39:04 +0200

    Fix filename in gen_ach_token

--- gen_ach_token.py
+++ gen_ach_token.py
@@ -2,10 +2,10 @@
 
 from banks.client import *
 
-with open("token.txt", "w") as fp:
+with open("token.js", "w") as fp:
     fp.write(
         f'module.exports = {{ token: "{dotenv.get_key(dotenv_file, "TOKEN")}" }}'
     )
 
 print("Test by doing:")
-print(f"ach -t token.js --url {os.environ.get('BASE_URL', '<your cozy URL>')} export io.cozy.banks.accounts")
+print(f"ach -t token.js --url {os.environ.get('BASE_URL', '<your cozy URL>')} export io.cozy.bank.accounts")
