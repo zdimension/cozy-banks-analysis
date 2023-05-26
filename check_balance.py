a4fa8dccba700172f8558f932d1a5d668820ffde
Tom Niget <tom.niget@etu.univ-cotedazur.fr>
2023-05-26 21:01:48 +0200

    Add --until parameter to check_balance

--- check_balance.py
+++ check_balance.py
@@ -3,11 +3,14 @@ from itertools import groupby
 
 from tabulate import tabulate
 
-from banks.client import get_operations, get_accounts, parse_args
+from banks.client import get_operations, get_accounts, parse_args, parser
 
-parse_args()
+parser.add_argument("--until", "-u", help="Only show operations until this date")
+args = parse_args()
 accounts = get_accounts()
 operations = get_operations()
+if args.until:
+    operations = [op for op in operations if op["date"] <= args.until]
 operations.sort(key=lambda o: o["account"])
 acc_op = {k: list(v) for k, v in groupby(operations, lambda o: o["account"])}
 
@@ -16,7 +19,7 @@ print(tabulate(
         (
             acc["__displayLabel"],
             b := acc["balance"],
-            cb := sum(u["amount"] for u in acc_op[acc["id"]]),
+            cb := sum(u["amount"] for u in acc_op.get(acc["id"], ())),
             round(b - cb + 1e-7, 2)
         )
         for acc in accounts
