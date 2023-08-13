fdddf2996e845985d59f3a51992b79485fe10687
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-08-13 15:59:48 +0200

    feat(check_balance): make curconv optional

--- check_balance.py
+++ check_balance.py
@@ -11,6 +11,14 @@ import currency_converter
 parser.add_argument("--until",
                     "-u",
                     help="Only show operations until this date")
+
+parser.add_argument(
+    "--curconv",
+    "-C",
+    help=
+    "Convert currencies to EUR. Note that this will not be completely acurate.",
+    action="store_true")
+
 args = parse_args()
 accounts = get_accounts()
 operations = get_operations()
@@ -18,20 +26,26 @@ if args.until:
     operations = [op for op in operations if op["date"] <= args.until]
 operations.sort(key=lambda o: o["account"])
 
-# convert currency to EUR
-for op in operations:
-    if op["currency"] == "EUR":
-        continue
 
-    cc = currency_converter.CurrencyConverter(fallback_on_missing_rate=True,
-                                              fallback_on_wrong_date=True)
+def convert_ops(operations):
+    # convert currency to EUR
+    for op in operations:
+        if op["currency"] == "EUR":
+            continue
 
-    date = datetime.datetime.fromisoformat(op["date"])
-    eur_amount = cc.convert(op["amount"], op["currency"], "EUR", date=date)
-    print(f"{op['amount']} {op['currency']} -> {eur_amount} EUR")
+        cc = currency_converter.CurrencyConverter(
+            fallback_on_missing_rate=True, fallback_on_wrong_date=True)
 
-    op["amount"] = eur_amount
-    op["currency"] = "EUR"
+        date = datetime.datetime.fromisoformat(op["date"])
+        eur_amount = cc.convert(op["amount"], op["currency"], "EUR", date=date)
+        print(f"{op['amount']} {op['currency']} -> {eur_amount} EUR")
+
+        op["amount"] = eur_amount
+        op["currency"] = "EUR"
+
+
+if args.curconv:
+    convert_ops(operations)
 
 acc_op = {k: list(v) for k, v in groupby(operations, lambda o: o["account"])}
 
